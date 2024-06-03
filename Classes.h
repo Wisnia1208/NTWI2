1908c857ea8932a77fd24cd4ccec531a43c73f32
Jakub Wisniewski <kwisniew12@gmail.com>
2024-06-03 23:48:29 +0200

    zrobiono porządek KLASY -> Classes.h

---
+++ Classes.h
@@ -0,0 +1,47 @@
+#pragma once
+
+class Car {
+protected:
+    std::string model = "";
+    int capacity = 500;
+public:
+    void setModel(std::string name) {
+        model = name;
+    }
+    std::string getModel() {
+        return model;
+    }
+
+    void setCapacity(int x) {
+        try {
+            if (x >= 0) {
+                capacity = x;
+            }
+            else {
+                throw(x);
+            }
+        }
+        catch (int e) {
+            std::cout << "error setCapacity() - input: " << e << " is below 0\n";
+        }
+    }
+    int getCapacity() {
+        return capacity;
+    }
+
+    Car(std::string name, int cap) {
+        model = name;
+        if (cap >= 0) {
+            capacity = cap;
+        }
+        else {
+            std::cout << "incorrect input CAPACITY\n";
+        }
+    }
+
+    Car(std::string name) {
+        model = name;
+    }
+
+    ~Car() {}
+};
--- NTWI2.cpp
+++ NTWI2.cpp
@@ -5,51 +5,9 @@
 #include <string>
 #include <vector>
 
-class Car {
-protected:
-    std::string model="";
-    int capacity = 500;
-public:
-    void setModel(std::string name) {
-        model = name;
-    }
-    std::string getModel() {
-        return model;
-    }
+#include "Classes.h"
 
-    void setCapacity(int x) {
-        try {
-            if (x >= 0) {
-                capacity = x;
-            }
-            else {
-                throw(x);
-            }
-        }
-        catch (int e){
-            std::cout << "error setCapacity() - input: " << e << " is below 0\n";
-        }
-    }
-    int getCapacity() {
-        return capacity;
-    }
 
-    Car(std::string name, int cap) {
-        model = name;
-            if (cap >= 0) {
-                capacity = cap;
-            }
-            else { 
-                std::cout << "incorrect input CAPACITY\n";
-            }
-    }
-
-    Car(std::string name) {
-        model = name;
-    }
-
-    ~Car() {}
-};
 
 std::vector <Car> carVector;
 
